3fbeb96f4f1d3903bac7e66ef6fb8e5886194a09
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-03-15 16:23:23 -0300

    state sem use

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -17,7 +17,7 @@ interface UserData {
 }
 
 const TelaAdmin = () => {
-  const [userData, setUserData] = useState<UserData[]>([]);
+  const [_userData, setUserData] = useState<UserData[]>([]);
   const [allUserData, setAllUserData] = useState<UserData[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(4);
